81d4f1282ddcdd0af4d4b840c4e5e0c645c6419d
CHI <jack840709@yahoo.com.tw>
2019-06-26 17:49:49 +0800

    apply to Mali-T860

---
+++ conv.cl
@@ -0,0 +1,4 @@
+__kernel void convolution(__global const float* input, __global const float* weight, __global float* output)
+{
+	
+}
---
+++ conv_mali.cpp
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+using namespace std;
+
+bool setup_gpu()
+{
+	cl_int err;
+	cl_uint num;
+	err = clGetPlatformIDs(0, NULL, &num);
+	if(err != CL_SUCCESS)
+	{
+		printf("clGetPlatformIDs() Error !!!\n");
+		cout << "Error Message :" << err << endl;
+		return false;
+	}
+	cout << "Platform number :" << num << endl;
+
+	vector<cl_platform_id> platforms(num);
+	err = clGetPlatformIDs(num, &platforms[0], NULL);
+	if(err != CL_SUCCESS)
+	{
+		printf("clGetPlatformIDs() Error !!!\n");
+		cout << "Error Message :" << err << endl;
+		return false;
+	}
+
+	err = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_DEFAULT, 0, NULL, &num);
+	if(err != CL_SUCCESS)
+	{
+		printf("clGetDeviceIDs() Error !!!\n");
+		cout << "Error Message :" << err << endl;
+		return false;
+	}
+	cout << "Device number :" << num << endl;
+
+	vector<cl_device_id> devices(num);
+	err = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_DEFAULT, num, &devices[0], NULL);
+	if(err != CL_SUCCESS)
+	{
+		printf("clGetDeviceIDs() Error !!!\n");
+		cout << "Error Message :" << err << endl;
+		return false;
+	}
+
+	for(int i = 0; i < num; i++)
+	{
+		size_t str_num;
+		string devname;
+		err = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 0, NULL, &str_num);
+		devname.resize(str_num);
+		err = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, str_num, &devname[0], NULL);
+		cout << devname.c_str() << endl;
+	}
+
+	cl_context context;
+	context = clCreateContext(NULL, num, &devices[0], NULL, NULL, &err);
+	if(err != CL_SUCCESS)
+	{
+		printf("clCreateContext() Error !!!\n");
+		cout << "Error Message :" << err << endl;
+		return false;
+	}
+
+	cl_command_queue queue;
+	queue = clCreateCommandQueue(context, devices[0], 0, &err);
+	if(err != CL_SUCCESS)
+	{
+		printf("clCreateCommandQueue() Error !!!\n");
+		cout << "Error Message :" << err << endl;
+		return false;
+	}
+
+	cl_program program;
+	program = clCreateProgramWithSource(context, );
+}
+
+int main(int argc, char* argv[])
+{
+	setup_gpu();
+	return 0;
+}
