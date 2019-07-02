30218753e8b02aba5fa988a65b9ed10e29677c4a
CHI <jack840709@yahoo.com.tw>
2019-07-02 16:59:50 +0800

    prototype

--- conv_mali.cpp
+++ conv_mali.cpp
@@ -1,9 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
+#include "dataflow.h"
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
 #else
@@ -13,6 +10,9 @@
 using namespace std;
 
 cl_device_id gpu_device;
+cl_context context;
+cl_command_queue queue;
+cl_kernel kernel;
 
 cl_program read_cl_file(char* file_name)
 {
@@ -28,50 +28,36 @@ cl_program read_cl_file(char* file_name)
 
 	const char* source = &data[0];
 	cl_program program;
-	program = clCreateProgramWithSource(context, 1, &source, NULL);
+	program = clCreateProgramWithSource(context, 1, &source, NULL, NULL);
 
 	return program;
 } 
 
-bool setup_gpu()
+void setup_gpu()
 {
 	cl_int err;
 	cl_uint num;
 	err = clGetPlatformIDs(0, NULL, &num);
 	if(err != CL_SUCCESS)
-	{
-		printf("clGetPlatformIDs() Error !!!\n");
-		cout << "Error Message :" << err << endl;
-		return false;
-	}
-	cout << "Platform number :" << num << endl;
+		print_error_message(err);
+	cout << "The number of platforms : " << num << endl;
 
 	vector<cl_platform_id> platforms(num);
 	err = clGetPlatformIDs(num, &platforms[0], NULL);
 	if(err != CL_SUCCESS)
-	{
-		printf("clGetPlatformIDs() Error !!!\n");
-		cout << "Error Message :" << err << endl;
-		return false;
-	}
+		print_error_message(err);
 
 	err = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_DEFAULT, 0, NULL, &num);
 	if(err != CL_SUCCESS)
-	{
-		printf("clGetDeviceIDs() Error !!!\n");
-		cout << "Error Message :" << err << endl;
-		return false;
-	}
-	cout << "Device number :" << num << endl;
+		print_error_message(err);
+	cout << "The number of devices : " << num << endl;
 
 	vector<cl_device_id> devices(num);
 	err = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_DEFAULT, num, &devices[0], NULL);
 	if(err != CL_SUCCESS)
-	{
-		printf("clGetDeviceIDs() Error !!!\n");
-		cout << "Error Message :" << err << endl;
-		return false;
-	}
+		print_error_message(err);
+
+	gpu_device = devices[0];
 
 	for(int i = 0; i < num; i++)
 	{
@@ -83,23 +69,13 @@ bool setup_gpu()
 		cout << devname.c_str() << endl;
 	}
 
-	cl_context context;
 	context = clCreateContext(NULL, num, &devices[0], NULL, NULL, &err);
 	if(err != CL_SUCCESS)
-	{
-		printf("clCreateContext() Error !!!\n");
-		cout << "Error Message :" << err << endl;
-		return false;
-	}
+		print_error_message(err);
 
-	cl_command_queue queue;
 	queue = clCreateCommandQueue(context, devices[0], 0, &err);
 	if(err != CL_SUCCESS)
-	{
-		printf("clCreateCommandQueue() Error !!!\n");
-		cout << "Error Message :" << err << endl;
-		return false;
-	}
+		print_error_message(err);
 }
 
 void print_device_info()
@@ -107,15 +83,77 @@ void print_device_info()
 
 }
 
-void compile_gpu_program()
+void print_error_message(cl_int err)
+{
+	cout << "Error Message :" << err << endl;
+	exit(-1);
+}
+
+void compile_gpu_program(float* I, float* W, float* O, config* data)
 {
 	cl_int err;
 	cl_program program = read_cl_file("conv.cl");
 	err = clBuildProgram(program, 1, &gpu_device, NULL, NULL, NULL);
+	if(err != CL_SUCCESS)
+		print_error_message(err);
+ 
+	kernel = clCreateKernel(program, "convolution", &err);
+	if(err != CL_SUCCESS)
+		print_error_message(err);
+
+	cl_mem buffer_I = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(cl_float)*4, I, NULL);
+	cl_mem buffer_W = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(cl_float)*4, W, NULL);
+	cl_mem buffer_O = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR, sizeof(cl_float)*4, O, NULL);
+
+	clSetKernelArg(kernel, 0, sizeof(cl_mem), &buffer_I);
+	clSetKernelArg(kernel, 1, sizeof(cl_mem), &buffer_W);
+	clSetKernelArg(kernel, 2, sizeof(cl_mem), &buffer_O);
+}
+
+void run_gpu_program(size_t global_work_size)
+{
+	cl_int err;
+	err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global_work_size, NULL, NULL, NULL, NULL);
+	if(err != CL_SUCCESS)
+		print_error_message(err);
+	err = clFinish(queue);
+	if(err != CL_SUCCESS)
+		print_error_message(err);
+}
+
+void run()
+{
+	config* data = (config*)malloc(sizeof(config));
+	data->input_size = 0;
+	data->input_c = 0;
+	data->weight_size = 0;
+	data->output_size = 0;
+	data->output_c = 0;
+	float* I = create_data(4);
+	float* W = create_data(4);
+	float* O = create_data(4);
+	clear_data(O, 4);
+
+	setup_gpu();
+	compile_gpu_program(I, W, O, data);
+	run_gpu_program(4);
+
+	//==============================================================//
+	for(int i = 0; i < 4; i++)
+	{
+		float result = 0.0;
+		result = (*(I+i)) * (*(W+i));
+		printf("cpu=%f\tgpu=%f\n", result, *(O+i));
+	}
+
+	free(O);
+	free(W);
+	free(I);
+	free(data);
 }
 
 int main(int argc, char* argv[])
 {
-	// setup_gpu();
+	run();
 	return 0;
 }
