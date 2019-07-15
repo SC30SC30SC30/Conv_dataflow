022db50a428c31193e52616cd05537166f9af004
chi <jack840709@yahoo.com.tw>
2019-07-16 06:02:32 +0800

    10807160602

--- conv_mali_test.cpp
+++ conv_mali_test.cpp
@@ -10,7 +10,7 @@ void run_tile_conv(cl_param* cl_gpu, config* data, tile_param* tile, size_t* glo
 	printf("total_I=%d\ttotal_W=%d\ttotal_O=%d\n", total_value(data, 'I'), total_value(data, 'W'), total_value(data, 'O'));
 
 	// prepare_to_execute(cl_gpu, false, "conv.cl", "convolution");
-	prepare_to_execute(cl_gpu, false, "conv.cl", "convolution_v2");
+	prepare_to_execute(cl_gpu, false, "conv.cl", "convolution");
 
 	float* gpu_I = malloc_gpu_space(cl_gpu, sizeof(float)*total_value(data, 'I'));
 	for(int i = 0; i < total_value(data, 'I'); i++)
@@ -35,103 +35,14 @@ void run_tile_conv(cl_param* cl_gpu, config* data, tile_param* tile, size_t* glo
 	start = clock();
 	tile_conv(cl_gpu, gpu_I, gpu_W, gpu_partsum, gpu_O, data, tile, global_work_size, local_work_size);
 	end = clock();
-	printf("clock = %ld ms\n", (end-start)*1000/CLOCKS_PER_SEC);
+	printf("gpu execution time = %ld ms\n", (end-start)*1000/CLOCKS_PER_SEC);
 
 	// the result of cpu computation
 	//==================================================================================================//
 	start = clock();
-	for(int oc = 0; oc < 384; oc++)
-	{
-		for(int ic = 0; ic < 192; ic++)
-		{
-			for(int oh = 0; oh < 13; oh++)
-			{
-				for(int ow = 0; ow < 13; ow++)
-				{
-					for(int kh = 0; kh < 3; kh++)
-					{
-						for(int kw = 0; kw < 3; kw++)
-						{
-							float partsum = *(I+ic*15*15+oh*15+ow+kh*15+kw) * *(W+oc*192*3*3+ic*3*3+kh*3+kw);
-							*(O+oc*13*13+oh*13+ow) += partsum;
-						}
-					}
-				}
-			}
-		}
-	}
+	direct_conv_oc_ic_oh_ow(I, W, O, data, tile, false, false, NULL, NULL, NULL);
 	end = clock();
-	printf("clock = %ld ms\n", (end-start)*1000/CLOCKS_PER_SEC);
-	//==================================================================================================//
-
-	// compare cpu and gpu result whether correct
-	//==================================================================================================//
-	for(int i = 0; i < 20; i++)
-	{
-		printf("%d.\tcpu=%f\tgpu_O=%f\n", i, *(O+i), *(gpu_O+i));
-	}
-	//==================================================================================================//
-
-	clean_objects(cl_gpu, gpu_I, gpu_W, gpu_O);
-	free(O);
-	free(W);
-	free(I);
-}
-
-void run_direct_conv(cl_param* cl_gpu, config* data, size_t* global_work_size, size_t* local_work_size)
-{
-	float* I = create_data(total_value(data, 'I'));
-	float* W = create_data(total_value(data, 'W'));
-	float* O = create_data(total_value(data, 'O'));
-	clear_data(O, total_value(data, 'O'));
-	printf("total_I=%d\ttotal_W=%d\ttotal_O=%d\n", total_value(data, 'I'), total_value(data, 'W'), total_value(data, 'O'));
-
-	prepare_to_execute(cl_gpu, false, "direct_conv.cl", "seq_convolution");
-
-	float* gpu_I = malloc_gpu_space(cl_gpu, sizeof(float)*total_value(data, 'I'));
-	for(int i = 0; i < total_value(data, 'I'); i++)
-	{
-		*(gpu_I+i) = *(I+i);
-	}
-	float* gpu_W = malloc_gpu_space(cl_gpu, sizeof(float)*total_value(data, 'W'));
-	for(int i = 0; i < total_value(data, 'W'); i++)
-	{
-		*(gpu_W+i) = *(W+i);
-	}
-	int partsum_num = data->output_c*data->output_size*data->output_size*data->input_c*data->weight_size*data->weight_size;
-	printf("partsum_num=%d\n", partsum_num);
-	float* gpu_partsum = malloc_gpu_space(cl_gpu, sizeof(float)*partsum_num);
-	float* gpu_O = malloc_gpu_space(cl_gpu, sizeof(float)*total_value(data, 'O'));
-	clear_data(gpu_O, total_value(data, 'O'));
-
-	clock_t start, end;
-	start = clock();
-	direct_conv(cl_gpu, gpu_I, gpu_W, gpu_partsum, gpu_O, data, global_work_size, local_work_size);
-	end = clock();
-	printf("clock = %ld\n", (end-start));
-
-	// the result of cpu computation
-	//==================================================================================================//
-	for(int oc = 0; oc < 384; oc++)
-	{
-		for(int ic = 0; ic < 192; ic++)
-		{
-			for(int oh = 0; oh < 13; oh++)
-			{
-				for(int ow = 0; ow < 13; ow++)
-				{
-					for(int kh = 0; kh < 3; kh++)
-					{
-						for(int kw = 0; kw < 3; kw++)
-						{
-							float partsum = *(I+ic*15*15+oh*15+ow+kh*15+kw) * *(W+oc*192*3*3+ic*3*3+kh*3+kw);
-							*(O+oc*13*13+oh*13+ow) += partsum;
-						}
-					}
-				}
-			}
-		}
-	}
+	printf("cpu execution time = %ld ms\n", (end-start)*1000/CLOCKS_PER_SEC);
 	//==================================================================================================//
 
 	// compare cpu and gpu result whether correct
@@ -162,7 +73,6 @@ int main(int argc, char* argv[])
 	printf("global_work_size=%ld\n", global_work_size);
 	printf("local_work_size=%ld\n", local_work_size);
 	printf("work_group_num=%ld\n", (global_work_size/local_work_size));
-	// run_direct_conv(cl_gpu, data, &global_work_size, &local_work_size);
 	run_tile_conv(cl_gpu, data, tile, &global_work_size, &local_work_size);
 
 	free(tile);
