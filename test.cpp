c660415cff45789f4bf0b76f8e540c66a0947728
chi <jack840709@yahoo.com.tw>
2019-09-18 10:40:12 +0800

    10809181040

--- test.cpp
+++ test.cpp
@@ -33,8 +33,9 @@ void run_one_case(config* data, tile_param* tile, int cache_size)
 		int tile_O = tile->tr * tile->tc * tile->tm;
 		printf("tile_I=%d\ttile_W=%d\ttile_O=%d\n", tile_I, tile_W, tile_O);
 
-		tile_conv_oh_ow_ic_oc(I, W, O, data, tile, 1, true, data_type, data_addr, &addr_idx);//IR
+		// tile_conv_oh_ow_ic_oc(I, W, O, data, tile, 1, true, data_type, data_addr, &addr_idx);//IR
 		// tile_conv_oc_ic_oh_ow(I, W, O, data, tile, 4, true, data_type, data_addr, &addr_idx);//WR
+		tile_conv_oh_ow_oc_ic(I, W, O, data, tile, 1, true, data_type, data_addr, &addr_idx);//OR
 		// direct_conv_oc_ic_oh_ow(I, W, O, data, tile, false, true, data_type, data_addr, &addr_idx);
 
 		// compute_rd(true, tile, data_type, data_addr, total_access);
@@ -166,8 +167,8 @@ int main(int argc, char* argv[])
 	tile_param* tile = (tile_param*)malloc(sizeof(tile_param));
 	// int a[10] = {31, 48, 5, 27, 256, 16, 16, 4, 4, 1};   // AlexNet的CONV2
 	// int a[10] = {15, 256, 3, 13, 384, 13, 13, 256, 3, 1};   // AlexNet的CONV3
-	// int a[10] = {15, 192, 3, 13, 384, 13, 13, 192, 8, 1};   // AlexNet的CONV4
-	int a[10] = {15, 192, 3, 13, 256, 1, 1, 1, 256, 1};   // AlexNet的CONV5
+	int a[10] = {15, 192, 3, 13, 384, 13, 13, 192, 8, 1};   // AlexNet的CONV4
+	// int a[10] = {15, 192, 3, 13, 256, 1, 1, 1, 256, 1};   // AlexNet的CONV5
 	// int a[10] = {8, 8, 1, 8, 16, 2, 2, 4, 2, 1};   // simple test 
 	set_configuration(data, tile, a);
 
